e39e8f088cb393c48302ad63646e36d150cb6238
A. <65830011+uflak09842@users.noreply.github.com>
2024-04-08 18:50:53 +0300

    main files

--- Toxicity/Assets/cods/Enemy.cs
+++ Toxicity/Assets/cods/Enemy.cs
@@ -1,27 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
-    //rivate GameObject redParticles;
+    
 
     public Rigidbody2D rb;
 
     public float health = 100f;
+<<<<<<< HEAD
+    private Animator anim;
+    EnemyAi ai;
+    public float life = 1;
+    
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        ai = GetComponent<EnemyAi>();
+        StartCoroutine(MyCoroutine());
+
+    }
+
+
+=======
 
     public int speed = 3;
+>>>>>>> 6b10d34b45725e5656aa3197a792e7033e02c7d0
 
     public void TakeDamage(float damage)
     {
+
+        StartCoroutine(MyCoroutine());
         health -= damage;
-        if(health <= 0)
+        anim.SetTrigger("enemyhurting");
+        if (health <= 0)
         {
-            //Instantiate(redParticles, transform.position, transform.rotation);
-            Destroy(gameObject);
+            if (anim != null)
+            {
+                ai.speed = 0;
+                anim.SetTrigger("enemydeathing");
+                          
+            }
+           
         }
     }
+<<<<<<< HEAD
+    void enemydestroyed()
+    {
+        Destroy(gameObject);
+    }
+    
+
+    IEnumerator MyCoroutine()
+    {
+        ai.speed *= 0.0001f;
+        yield return new WaitForSeconds(0.2f);
+        ai.speed *=10000;
+
+=======
     // Start is called before the first frame update
     void Start()
     {
@@ -32,5 +71,6 @@ public class Enemy : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(speed, rb.velocity.y);
+>>>>>>> 6b10d34b45725e5656aa3197a792e7033e02c7d0
     }
 }
